fc93bbb13df7b53980edb0960685b4f794e925e3
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-04-11 16:55:15 -0400

    Starting on Products Table

--- app/models/products.py
+++ app/models/products.py
@@ -1,2 +1,13 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy import func
 
+class Product(db.Model):
+    __table__ = 'products'
+
+    if environment == 'products':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    
