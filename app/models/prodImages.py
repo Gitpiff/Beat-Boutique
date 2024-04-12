6f4e7117f213ecd014a5971ec666633e9fca9419
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-04-12 16:46:49 -0400

    Adding updated and created at for the product images table

--- app/models/prodImages.py
+++ app/models/prodImages.py
@@ -9,6 +9,8 @@ class ProductImage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, db.Foreign(add_prefix_for_prod('products.id')), nullable=False)
     image_url = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
 
     product = db.relationship('Product', back_populates='images')
 
@@ -16,5 +18,7 @@ class ProductImage(db.Model):
         return {
             'id': self.id,
             'product_id': self.product_id,
-            'image_url': self.image_url
+            'image_url': self.image_url,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
