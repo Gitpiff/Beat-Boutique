7c3491c5bd77179974044c1bca5a9fb313538875
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-04-12 16:09:55 -0400

    Finished Product Images Table

--- app/models/prodImages.py
+++ app/models/prodImages.py
@@ -6,4 +6,15 @@ class ProductImage(db.Model):
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
-    id = db.Column(db.Integer, db.Foreign(add_prefix_for_prod('products')))
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.Foreign(add_prefix_for_prod('products.id')), nullable=False)
+    image_url = db.Column(db.String(255), nullable=False)
+
+    product = db.relationship('Product', back_populates='images')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'product_id': self.product_id,
+            'image_url': self.image_url
+        }
