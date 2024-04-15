9e8728dad42387f9231a94fba9e33ed1d38afeae
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-04-15 09:54:39 -0400

    Added product table

---
+++ app/models/product.py
@@ -0,0 +1,31 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from sqlalchemy import func
+
+class Product(db.Model):
+    __table__ = 'products'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Numeric(10,2), nullable=False)
+    inventory = db.Column(db.Boolean, nullable=False, default=True)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
+
+    user = db.relationship('User', back_populates='products')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'owner_id': self.owner_id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'inventory': self.inventory,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
