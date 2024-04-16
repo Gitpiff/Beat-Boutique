840c11933547699b61e786217068bab1645c8689
Oscar Robles <oscarrsvp@gmail.com>
2024-04-15 21:32:35 -0500

    Added Relationships W ProductImage & Reviews Table

--- app/models/products.py
+++ app/models/products.py
@@ -1,31 +1,36 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy import func
 
-class Product(db.Model):
-    __tablename__ = 'products'
 
-    if environment == 'production':
-        __table_args__ = {'schema': SCHEMA}
+class Product(db.Model):
+    __tablename__ = "products"
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    owner_id = db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False
+    )
     name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text)
-    price = db.Column(db.Numeric(10,2), nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
     inventory = db.Column(db.Boolean, nullable=False, default=True)
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, onupdate=func.now())
 
-    user = db.relationship('User', back_populates='products')
+    user = db.relationship("User", back_populates="products")
+    images = db.relationship("ProductImage", back_populates="product")
+    reviews = db.relationship("Review", back_populates="product")
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'owner_id': self.owner_id,
-            'name': self.name,
-            'description': self.description,
-            'price': self.price,
-            'inventory': self.inventory,
-            'created_at': self.created_at,
-            'updated_at': self.updated_at
+            "id": self.id,
+            "owner_id": self.owner_id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "inventory": self.inventory,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
         }
