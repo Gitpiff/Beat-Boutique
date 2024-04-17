f5c765d0dad38ca7327fa60bb23c58800f5055e1
Oscar Robles <oscarrsvp@gmail.com>
2024-04-17 17:59:01 -0500

    Included New Routes & Methods

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 from ..forms.products import Products
 from ..models import db, Product
 
@@ -34,3 +34,49 @@ def create_new_product():
         return new_product.to_dict()
 
     return form.errors, 401
+
+
+@product_routes.route("/<int:product_id>")
+def get_product_by_id(product_id):
+    product = Product.query.get(product_id)
+
+    if not product:
+        return jsonify({"error": "Product not found"}), 400
+
+    return product.to_dict()
+
+
+@product_routes.route("/<int:product_id>", methods=["PUT"])
+# @login_required
+def update_products(product_id):
+    product = Product.query.get(product_id)
+
+    if not product:
+        return jsonify({"error": "Product not found"})
+
+    if current_user.id != product.owner_id:
+        return jsonify({"error": "unauthorized"})
+
+    data = request.json
+
+    if data:
+        product.name = data.get("name", product.name)
+        product.description = data.get("description", product.description)
+        product.price = data.get("price", product.price)
+        product.inventory = data.get("inventory", product.inventory)
+
+    db.session.commit()
+
+    return product.to_dict()
+
+
+@product_routes.route("/<int:product_id>", methods=["DELETE"])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+
+    if not product:
+        return jsonify({"error": "Product not found"})
+
+    db.session.delete(product)
+    db.session.commit()
+    return jsonify({"message": "Product was deleted successful"})
