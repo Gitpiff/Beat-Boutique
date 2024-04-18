00af9eb28caa7231b93b3130a977b478288c4b19
Oscar Robles <oscarrsvp@gmail.com>
2024-04-17 21:37:38 -0500

    Added ProductImages Routes

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,21 +1,31 @@
+from sqlalchemy.orm import joinedload
 from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
 from ..forms.products import Products
-from ..models import db, Product
+from ..forms.product_image import ProductImages
+from ..models import db, Product, ProductImage
 
 product_routes = Blueprint("products", __name__)
 
 
 @product_routes.route("/")
 def index():
-    get_all_products = db.session.query(Product).all()
+    """
+    Gets all products
+    """
+    get_all_products = Product.query.options(joinedload("images")).all()
+
     products_dict = [product.to_dict() for product in get_all_products]
+
     return jsonify(products_dict)
 
 
 @product_routes.route("/", methods=["POST"])
 @login_required
 def create_new_product():
+    """
+    Creates new product
+    """
     form = Products()
 
     form.csrf_token.data = request.cookies["csrf_token"]
@@ -38,6 +48,9 @@ def create_new_product():
 
 @product_routes.route("/<int:product_id>")
 def get_product_by_id(product_id):
+    """
+    Get product by Id
+    """
     product = Product.query.get(product_id)
 
     if not product:
@@ -49,6 +62,9 @@ def get_product_by_id(product_id):
 @product_routes.route("/<int:product_id>", methods=["PUT"])
 @login_required
 def update_products(product_id):
+    """
+    Updates product by Id
+    """
     product = Product.query.get(product_id)
 
     if not product:
@@ -73,6 +89,9 @@ def update_products(product_id):
 @product_routes.route("/<int:product_id>", methods=["DELETE"])
 @login_required
 def delete_product(product_id):
+    """
+    Deletes product
+    """
     product = Product.query.get(product_id)
 
     if not product:
@@ -84,3 +103,52 @@ def delete_product(product_id):
     db.session.delete(product)
     db.session.commit()
     return jsonify({"message": "Product was deleted successful"})
+
+
+@product_routes.route("/images/<int:product_id>", methods=["POST"])
+def add_new_image(product_id):
+    """
+    Creates new product image
+    """
+    product = Product.query.get(product_id)
+
+    if not product:
+        return jsonify({"error": "Product not found"}), 400
+
+    form = ProductImages()
+
+    form.csrf_token.data = request.cookies["csrf_token"]
+    if form.validate_on_submit():
+        new_image = ProductImage(
+            product_id=product.to_dict()["id"], image_url=form.data["image_url"]
+        )
+
+        db.session.add(new_image)
+        db.session.commit()
+
+        return new_image.to_dict()
+
+    return form.errors, 401
+
+
+@product_routes.route("/images/<int:image_id>", methods=["DELETE"])
+@login_required
+def delete_image(image_id):
+    """
+    Deletes product image
+    """
+
+    image_product = ProductImage.query.get(image_id)
+
+    if not image_product:
+        return jsonify({"error": "Product Image not found"}), 400
+
+    products = Product.query.get(image_product.to_dict()["product_id"])
+
+    if current_user.id != products.to_dict().owner_id:
+        return jsonify({"error": "unauthorized"})
+
+    db.session.delete(image_product)
+    db.session.commit()
+
+    return jsonify({"message": "Product Image was deleted successful"})
