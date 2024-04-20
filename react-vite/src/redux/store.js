06c93ac92e054dde9d03543ab00f1343e000d8d2
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-04-20 17:28:45 -0400

    Revert "Merge branch 'dev' of https://github.com/Gitpiff/Beat-Boutique into dev"
    This reverts commit 7ea3038d94a7ca51e3e39444d74aa898eec6f1f4, reversing
    changes made to 629a344dd0366c1f0e014341a56d2ba870e4d85e.
    
    git commit -m "Revert merge commit 7ea3038"#

--- react-vite/src/redux/store.js
+++ react-vite/src/redux/store.js
@@ -3,24 +3,21 @@ import {
   applyMiddleware,
   compose,
   combineReducers,
-} from 'redux';
-import thunk from 'redux-thunk';
-import sessionReducer from './session';
-import productReducer from './products';
-import reviewReducer from './reviews';
+} from "redux";
+import thunk from "redux-thunk";
+import sessionReducer from "./session";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
-  products: productReducer,
-  reviews: reviewReducer,
 });
 
 let enhancer;
-if (import.meta.env.MODE === 'production') {
+if (import.meta.env.MODE === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = (await import('redux-logger')).default;
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const logger = (await import("redux-logger")).default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
