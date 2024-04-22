c1b3d7a60cccc3009560f33c893899c5e3b5a05b
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-04-22 19:43:04 -0400

    Added the hamburger icon for loging and sign up also added useState so functionality has the hamburger when not logged in but profile button when logged in

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,38 +1,52 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 import "./Navigation.css";
 
 function Navigation() {
-
   const user = useSelector((state) => state.session.user);
+  const [showMenu, setShowMenu] = useState(false);
 
-  
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <nav>
-    <div className="logo">
-      <NavLink to="/">BeatBoutique</NavLink>
-    </div>
-    <div className="search-bar">
-      <input type="text" placeholder="Search..." />
-    </div>
-    <ul>
-      {user ? (
-        <li>
-          <ProfileButton />
-        </li>
-      ) : (
-        <>
+      <div className="logo">
+        <NavLink to="/">BeatBoutique</NavLink>
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search..." />
+      </div>
+      <ul>
+        {user ? (
           <li>
-            <NavLink to="/signup">Sign Up</NavLink>
+            <ProfileButton />
           </li>
-          <li>
-            <NavLink to="/login">Log In</NavLink>
-          </li>
-        </>
-      )}
-    </ul>
-  </nav>
+        ) : (
+          <>
+            <li>
+              <button className="fabars" onClick={toggleMenu}>
+                <FaBars />
+              </button>
+            </li>
+            {showMenu && (
+              <div className="menu">
+                <li>
+                  <NavLink to="/signup">Sign Up</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/login">Log In</NavLink>
+                </li>
+              </div>
+            )}
+          </>
+        )}
+      </ul>
+    </nav>
   );
 }
 
