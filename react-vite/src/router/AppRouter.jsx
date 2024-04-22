17eea1d3b2d1b9c03f32ed8b87844c5177baaea1
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-04-22 18:27:01 -0400

    After discussing with Oscar and Victor

--- react-vite/src/router/AppRouter.jsx
+++ react-vite/src/router/AppRouter.jsx
@@ -1,30 +1,29 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';import LoginFormPage from '../components/LoginFormPage';
-import SignupFormPage from '../components/SignupFormPage';
-import Layout from './Layout';
-import LandingPage from '../components/LandingPage';
-import ProductDetails from '../components/ProductDetails';
-import CreateProduct from '../components/CreateProduct';
-import CreateReview from '../components/CreateReview';
-import ManageProducts from '../components/ManageProducts';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginFormPage from "../components/LoginFormPage";
+import SignupFormPage from "../components/SignupFormPage";
+import Layout from "./Layout";
+import LandingPage from "../components/LandingPage";
+import ProductDetails from "../components/ProductDetails";
+import CreateProduct from "../components/CreateProduct";
+import CreateReview from "../components/CreateReview";
+import ManageProducts from "../components/ManageProducts";
 
-// function Layout() {
-//   const dispatch = useDispatch();
-//   const [isLoaded, setIsLoaded] = useState(false);
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
 
-//   useEffect(() => {
-//     dispatch(sessionActions.restoreUser()).then(() => {
-//       setIsLoaded(true)
-//     });
-//   }, [dispatch]);
-//   return (
-//     <>
-//       <Navigation isLoaded={isLoaded} />
-//       {isLoaded && <Outlet />}
-//     </>
-//   );
-// }
-
-
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true);
+    });
+  }, [dispatch]);
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
 
 export const router = createBrowserRouter([
   {
@@ -44,43 +43,31 @@ export const router = createBrowserRouter([
       },
       {
         path: "products/:id",
-        element: <ProductDetails />
+        element: <ProductDetails />,
       },
       {
         path: "products/current",
-        element: <ManageProducts />
+        element: <ManageProducts />,
       },
       {
         path: "products/new",
-        element: <CreateProduct />
+        element: <CreateProduct />,
       },
       {
         path: "review/new",
-        element: <CreateReview />
-      }
+        element: <CreateReview />,
+      },
+      {
+        path: "products/:id/edit",
+        element: <UpdateProduct />,
+      },
+      {
+        path: "reviews/:id/edit",
+        element: <UpdateReview />,
+      },
     ],
   },
 ]);
-
-//Do we need one for updating and/or deleting a product or a review?
-
-// {
-//   path: "products/:id/edit",
-//   element: <UpdateProduct />,
-// },
-// {
-//   path: "products/:id/delete",
-//   element: <DeleteProduct />,
-// },
-// {
-//   path: "reviews/:id/edit",
-//   element: <UpdateReview />,
-// },
-// {
-//   path: "reviews/:id/delete",
-//   element: <DeleteReview />,
-// },
-
 
 function App() {
   return <RouterProvider router={router} />;
