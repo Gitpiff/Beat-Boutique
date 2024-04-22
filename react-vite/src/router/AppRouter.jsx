b2b9253dfde170676344377906456e36827f314c
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-04-22 17:17:07 -0400

    Adding more routes

--- react-vite/src/router/AppRouter.jsx
+++ react-vite/src/router/AppRouter.jsx
@@ -1,9 +1,11 @@
-import { createBrowserRouter } from 'react-router-dom';
-import LoginFormPage from '../components/LoginFormPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
 import Layout from './Layout';
 import LandingPage from '../components/LandingPage';
 import ProductDetails from '../components/ProductDetails';
+import CreateProduct from '../components/CreateProduct';
+import CreateReview from '../components/CreateReview';
+import ManageProducts from '../components/ManageProducts';
 
 
 export const router = createBrowserRouter([
@@ -25,9 +27,45 @@ export const router = createBrowserRouter([
       {
         path: "products/:id",
         element: <ProductDetails />
+      },
+      {
+        path: "products/current",
+        element: <ManageProducts />
+      },
+      {
+        path: "products/new",
+        element: <CreateProduct />
+      },
+      {
+        path: "review/new",
+        element: <CreateReview />
       }
     ],
   },
 ]);
 
-//Do we need one for writing a review?
+//Do we need one for updating and/or deleting a product or a review?
+
+// {
+//   path: "products/:id/edit",
+//   element: <UpdateProduct />,
+// },
+// {
+//   path: "products/:id/delete",
+//   element: <DeleteProduct />,
+// },
+// {
+//   path: "reviews/:id/edit",
+//   element: <UpdateReview />,
+// },
+// {
+//   path: "reviews/:id/delete",
+//   element: <DeleteReview />,
+// },
+
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
