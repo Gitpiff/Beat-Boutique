253726017152c11cf5d63f0f5964bddfd14e7d82
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2024-04-22 13:18:21 -0400

    Added routes to AppRouter

--- react-vite/src/router/AppRouter.jsx
+++ react-vite/src/router/AppRouter.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from 'react-router-dom';
 import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
 import Layout from './Layout';
+import LandingPage from '.components/LandingPage';
+import ProductDetails from '../components/ProductDetails';
+
 
 export const router = createBrowserRouter([
   {
@@ -9,7 +12,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Welcome!</h1>,
+        element: <LandingPage />,
       },
       {
         path: "login",
@@ -19,6 +22,10 @@ export const router = createBrowserRouter([
         path: "signup",
         element: <SignupFormPage />,
       },
+      {
+        path: "products/:id",
+        element: <ProductDetails />
+      }
     ],
   },
 ]);
