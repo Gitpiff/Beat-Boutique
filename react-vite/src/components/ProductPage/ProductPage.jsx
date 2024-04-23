ff7d478d24f5411bbf9a50a4a9251a0bc9c41eb5
Victor <viknavarro@gmail.com>
2024-04-23 23:25:30 +0100

    need work on reviews

--- react-vite/src/components/ProductPage/ProductPage.jsx
+++ react-vite/src/components/ProductPage/ProductPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductById  } from "../../redux/products";
+import { getProductReview } from "../../redux/reviews";
 
 const ProductPage = () => {
 
@@ -11,11 +12,21 @@ const ProductPage = () => {
 
     // Get Product from redux store
     const product = useSelector(state => state.products ? state.products[productId] : null);
+
+    // Get Reviews
+    const reviews = Object.values(useSelector(state => state.reviews));
+
+    // Get current user
+    const currentUser = useSelector(state => state.session.user);
     
     // Get selected Product
     useEffect(() => {
-        dispatch(getProductById(product))
-    }, [dispatch, product])
+        dispatch(getProductById(productId));
+
+        if(reviews.length) {
+            dispatch(getProductReview(productId));
+        }
+    }, [dispatch, productId, reviews.length])
 
 
     return (
@@ -25,6 +36,25 @@ const ProductPage = () => {
             <h2>{product.price}</h2>
             <h3>{product.inventory}</h3>
             <img src={product.image_url} alt={product.name} />
+            <div>
+                <h4>&#9733;{product.avgRating !== "No reviews found" ? `${parseFloat(product.avgRating).toFixed(2)}`: "New"}</h4>
+                {product.numReviews !== 0 && (
+                    <span>
+                        {product.numReviews && product.numReviews === 1 ? '1 review' : 'reviews'}
+                    </span>
+                )}
+            </div>
+            {/* If more than One Review display Reviews instead of Review */}
+            {(currentUser && product.numReviews === 0 && currentUser.id !== product.owner_id) &&
+            <>
+                <button>Be the first to post a review!</button>
+            </>
+            }
+
+            {/* {()
+
+            } */}
+
         </>
     )
 }
