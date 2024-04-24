c7025f17148c5ef6d00e5f7e0797d17a99bd64a9
Oscar Robles <oscarrsvp@gmail.com>
2024-04-23 20:28:25 -0500

    Refactor component

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,25 +1,16 @@
-import { NavLink } from "react-router-dom";
-import ProfileButton from "./ProfileButton";
-import { useSelector } from "react-redux";
-import { useState } from "react";
-import { FaBars } from "react-icons/fa";
-import "./Navigation.css";
+import { NavLink } from 'react-router-dom';
+import ProfileButton from './ProfileButton';
+import { useSelector } from 'react-redux';
+import './Navigation.css';
 
 function Navigation() {
   const user = useSelector((state) => state.session.user);
-  const [showMenu, setShowMenu] = useState(false);
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
 
   return (
     <nav>
       <div className="logo">
         <NavLink to="/">BeatBoutique</NavLink>
-      </div>
-      <div className="search-bar">
-        <input type="text" placeholder="Search..." />
+        <input className="search-bar" type="text" placeholder="Search..." />
       </div>
       <ul>
         {user ? (
@@ -28,21 +19,14 @@ function Navigation() {
           </li>
         ) : (
           <>
-            <li>
-              <button className="fabars" onClick={toggleMenu}>
-                <FaBars />
-              </button>
-            </li>
-            {showMenu && (
-              <div className="menu">
-                <li>
-                  <NavLink to="/signup">Sign Up</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/login">Log In</NavLink>
-                </li>
-              </div>
-            )}
+            <div className="menu">
+              <li>
+                <NavLink to="/signup">Sign Up</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">Log In</NavLink>
+              </li>
+            </div>
           </>
         )}
       </ul>
